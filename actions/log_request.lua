44941dcbbe6f16f77d95815510710b35068e74ea
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-10-07 19:35:35 +0200

    autocreating logs dir

--- actions/log_request.lua
+++ actions/log_request.lua
@@ -6,6 +6,7 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()    
 
 local log_text = debug.print_req_info_return(req)
+os.execute("mkdir -p logs/requests")
 local log_file = io.open("logs/requests/" .. uuid_saved, "w")
 
 
--- actions/log_response.lua
+++ actions/log_response.lua
@@ -7,7 +7,7 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()
 
 local log_text = debug.print_res_info_return(req)
-
+os.execute("mkdir -p logs/responses")
 local log_file = io.open("logs/responses/" .. uuid_saved, "w")
 
 log_file:write(log_text)

cd463e654b6a5f223c887ce75505440349126210
Mitchell Tannenbaum <tannenbaum.mitchell@gmail.com>
2018-10-07 20:39:37 +0000

    Update log_response.lua

--- actions/log_response.lua
+++ actions/log_response.lua
@@ -7,8 +7,8 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()
 
 local log_text = debug.print_res_info_return(req)
-os.execute("mkdir -p logs/responses")
-local log_file = io.open("logs/responses/" .. uuid_saved, "w")
+os.execute("mkdir -p log/response")
+local log_file = io.open("log/response/" .. uuid_saved, "w")
 
 log_file:write(log_text)
 log_file:close()                                

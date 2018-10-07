9f8a720f089877216d33b3633b7e55431ea68024
Mitchell Tannenbaum <tannenbaum.mitchell@gmail.com>
2018-10-07 20:39:07 +0000

    Update log_request.lua

--- actions/log_request.lua
+++ actions/log_request.lua
@@ -6,8 +6,8 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()    
 
 local log_text = debug.print_req_info_return(req)
-os.execute("mkdir -p logs/requests")
-local log_file = io.open("logs/requests/" .. uuid_saved, "w")
+os.execute("mkdir -p log/request")
+local log_file = io.open("log/request/" .. uuid_saved, "w")
 
 
 log_file:write(log_text)

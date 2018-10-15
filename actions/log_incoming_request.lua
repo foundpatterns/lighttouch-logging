7137f3758d9f88108cf0533a8bdd10632b1f5563
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-10-15 00:16:23 -0400

    change event names

--- actions/log_incoming_request.lua
+++ actions/log_incoming_request.lua
@@ -1,4 +1,4 @@
-event: ["logRequest"]
+event: ["incoming_request_received"]
 priority: 1
 
 local debug = require "debug"
@@ -6,8 +6,8 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()    
 
 local log_text = debug.print_request_info_return(request)
-os.execute("mkdir -p log/request")
-local log_file = io.open("log/request/" .. uuid_saved, "w")
+os.execute("mkdir -p log/incoming-request")
+local log_file = io.open("log/incoming-request/" .. uuid_saved, "w")
 
 
 log_file:write(log_text)
--- actions/log_outgoing_response.lua
+++ actions/log_outgoing_response.lua
@@ -1,4 +1,4 @@
-event: ["response_process"]
+event: ["outgoing_response_about_to_be_sent"]
 priority: 1
 
 local debug = require "debug"
@@ -7,13 +7,13 @@ local debug = require "debug"
 local uuid_saved = debug.uuid_return()
 
 if not response then
-  log.error("response_process trigerred but no response available")
+  log.error("outgoing_response_about_to_be_sent trigerred but no response available")
   return
 end
 
 local log_text = debug.print_res_info_return(response)
-os.execute("mkdir -p log/response")
-local log_file = io.open("log/response/" .. uuid_saved, "w")
+os.execute("mkdir -p log/outgoing-response")
+local log_file = io.open("log/outgoing-response/" .. uuid_saved, "w")
 
 log_file:write(log_text)
 log_file:close()                                

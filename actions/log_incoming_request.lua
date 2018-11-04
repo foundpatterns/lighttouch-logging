dc79eb5f846fa47bcaac6fb4686504d39c0c3076
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-04 00:33:41 -0400

    Use generated request uuid instead of creating one

--- actions/log_incoming_request.lua
+++ actions/log_incoming_request.lua
@@ -2,14 +2,9 @@ event: ["incoming_request_received"]
 priority: 1
 input_parameters: ["request"]
 
-
-local uuid_saved = uuid.v4()
-
 local log_text = yaml.from_table(request)
 fs.create_dir("log/incoming-request", true)
-local log_file = io.open("log/incoming-request/" .. uuid_saved, "w")
+local log_file = io.open("log/incoming-request/" .. request.uuid, "w")
 
 log_file:write(log_text)
 log_file:close()
-
-log.info("Logged incoming request as " .. uuid_saved)

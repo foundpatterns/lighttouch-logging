e0af96c0cf444bb9fd5363a5d31994da46e2eb96
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-09 00:27:52 -0400

    Disable log content stores

--- actions/setup_content_stores.lua
+++ actions/setup_content_stores.lua
@@ -2,6 +2,7 @@ event: ["lighttouch_loaded"]
 priority: 50
 input_parameters: []
 
+do return end -- Temporarily disable this action until we sort logs out
 content.stores.incoming_request = "log/incoming-request/"
 content.stores.outgoing_request = "log/outgoing-request/"
 content.stores.outgoing_response = "log/outgoing-response/"

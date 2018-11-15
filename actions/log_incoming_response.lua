b655000e72afaa73e7f0a5cbc71fd39dec53ea6c
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-15 01:02:52 -0400

    Update client request logging actions

--- actions/log_incoming_response.lua
+++ actions/log_incoming_response.lua
@@ -2,9 +2,9 @@ event: ["incoming_response_received"]
 priority: 1
 input_parameters: ["response"]
 
-local log_text = yaml.from_table(response)
-fs.create_dir("log/incoming-response", true)
-local log_file = io.open("log/incoming-response/" .. request.uuid, "w")
+local fields = table.copy(response)
+fields.model = "incoming-response"
+fields.body = nil
+fields.body_raw = nil
 
-log_file:write(log_text)
-log_file:close()
+content.write_file("incoming-response", response.uuid, fields, response.body_raw)
--- actions/log_outgoing_request.lua
+++ actions/log_outgoing_request.lua
@@ -2,9 +2,9 @@ event: ["outgoing_request_about_to_be_sent"]
 priority: 1
 input_parameters: ["request"]
 
-local log_text = yaml.from_table(request)
-fs.create_dir("log/outgoing-request", true)
-local log_file = io.open("log/outgoing-request/" .. request.uuid, "w")
+local fields = table.copy(request)
+fields.model = "outgoing-request"
+fields.body = nil
+fields.body_raw = nil
 
-log_file:write(log_text)
-log_file:close()
+content.write_file("outgoing-request", request.uuid, fields, request.body_raw)
